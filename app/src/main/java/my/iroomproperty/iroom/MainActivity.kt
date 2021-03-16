075555a72e58e355fbb0c40bb9d7fec141ca2b56
ibilikstar <80731618+ibilikstar@users.noreply.github.com>
2021-03-16 13:20:54 +0800

    Code completion as per CEO's instructions

--- app/src/main/java/my/iroomproperty/iroom/MainActivity.kt
+++ app/src/main/java/my/iroomproperty/iroom/MainActivity.kt
@@ -2,11 +2,13 @@ package my.iroomproperty.iroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import my.iroomproperty.iroom_modelar.utils.PropertyFilters
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        PropertyFilters.ROOM_RENTAL
     }
 }
